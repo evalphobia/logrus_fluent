e5e48289b23a60e9e4961219fe4f30280fba1552
evalphobia <evalphobia@gmail.com>
2015-07-07 11:49:36 +0900

    Create fluentd hook for logrus

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# logrus_fluent
-fluentd hooks for logrus 
+# Fluentd Hook for Logrus <img src="http://i.imgur.com/hTeVwmJ.png" width="40" height="40" alt=":walrus:" class="emoji" title=":walrus:"/>
+
+## Usage
+
+```go
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/evalphobia/logrus_fluent"
+)
+
+func main() {
+	hook := logrus_fluent.NewHook("localhost", 24224)
+	hook.SetLevels([]logrus.Level{
+		logrus.PanicLevel,
+		logrus.ErrorLevel,
+	})
+
+	logrus.AddHook(hook)
+}
+```
+
+
+## Special fields
+
+Some logrus fields have a special meaning in this hook.
+
+- `tag` is used as a fluentd tag. (if `tag` is omitted, Entry.Message is used as a fluentd tag)
---
+++ fluent.go
@@ -0,0 +1,77 @@
+package logrus_fluent
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/fluent/fluent-logger-golang/fluent"
+)
+
+const (
+	TagName  = "fluent"
+	TagField = "tag"
+)
+
+var defaultLevels = []logrus.Level{
+	logrus.PanicLevel,
+	logrus.FatalLevel,
+	logrus.ErrorLevel,
+	logrus.WarnLevel,
+	logrus.InfoLevel,
+}
+
+type fluentHook struct {
+	host   string
+	port   int
+	levels []logrus.Level
+}
+
+func NewHook(host string, port int) *fluentHook {
+	return &fluentHook{
+		host:   host,
+		port:   port,
+		levels: defaultLevels,
+	}
+}
+
+func getTagAndDel(entry *logrus.Entry) string {
+	var v interface{}
+	var ok bool
+	if v, ok = entry.Data[TagField]; !ok {
+		return entry.Message
+	}
+
+	var val string
+	if val, ok = v.(string); !ok {
+		return entry.Message
+	}
+	delete(entry.Data, TagField)
+	return val
+}
+
+func setLevelString(entry *logrus.Entry) {
+	entry.Data["level"] = entry.Level.String()
+}
+
+func (hook *fluentHook) Fire(entry *logrus.Entry) error {
+	logger, err := fluent.New(fluent.Config{
+		FluentHost: hook.host,
+		FluentPort: hook.port,
+	})
+	if err != nil {
+		return err
+	}
+	defer logger.Close()
+
+	setLevelString(entry)
+	tag := getTagAndDel(entry)
+	data := ConvertToValue(entry.Data, TagName)
+	err = logger.PostWithTime(tag, entry.Time, data)
+	return err
+}
+
+func (hook *fluentHook) Levels() []logrus.Level {
+	return hook.levels
+}
+
+func (hook *fluentHook) SetLevels(levels []logrus.Level) {
+	hook.levels = levels
+}
